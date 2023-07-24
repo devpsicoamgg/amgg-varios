45ddf19f8498376b03284687ce606e5f4b8738f1
Alberto Mario Guerrero G <psico.amgg@gmail.com>
2023-07-24 13:48:08 -0500

    Arrow Function () => {}

--- alberto_estudio/03_functions.js
+++ alberto_estudio/03_functions.js
@@ -24,7 +24,48 @@
     //***FUNCIÓN CON CONSTRUCTOR***      
     let sum = new Function(arg1, arg2, arg3, "cuerpo de la ƒ()");
 
-    //***FUNCIÓN FLECHA ->*** - NO NECESARIAMENTE EN UNA VARIABLE. NO RECOMENDADA EN OBJETOS. 
-    const sum = (a, b) => a + b;
+    //***FUNCIÓN FLECHA  O ARROW FUNCTION ->*** - NO NECESARIAMENTE EN UNA VARIABLE. NO FUNCIONA EN CLASES.
+    // La escritura es mas expresiva y estetica.  
+
+    // Forma básica de una Arrow Function con un solo parámetro y una sola línea de retorno
+        const sumar = (a, b) => a + b;
+
+// Arrow Function equivalente de una función tradicional
+// function suma(a, b) {
+//   return a + b;
+// }
+
+// TEMA: HERENCIA DE VALOR DE THIS EN ARROW FUNCTIONS
+
+const obj = {
+  nombre: "Juan",
+  // Función tradicional con valor de `this` vinculado al objeto `obj`
+  saludar: function() {
+    console.log("Hola, mi nombre es " + this.nombre);
+  },
+  // Arrow Function: No tiene su propio `this`, hereda el `this` del contexto en el que se define
+  saludarArrow: () => {
+    console.log("Hola, mi nombre es " + this.nombre); // En este caso, `this.nombre` sería undefined
+  }
+};
+
+obj.saludar(); // Output: Hola, mi nombre es Juan
+obj.saludarArrow(); // Output: Hola, mi nombre es undefined (porque `this.nombre` es undefined)
+
+// TEMA: LIMITACIONES DE ARROW FUNCTIONS
+
+// Función tradicional que puede usarse como constructor
+function Persona(nombre) {
+  this.nombre = nombre;
+}
+
+const persona1 = new Persona("Juan");
+
+// Arrow Function no puede usarse como constructor
+const PersonaArrow = (nombre) => {
+  this.nombre = nombre; // Esto daría un error porque `this` no está vinculado en Arrow Functions
+};
+
+const persona2 = new PersonaArrow("Ana"); // Esto daría un error
 
     //  En JavaScript, es común utilizar camelCase para nombrar variables y funciones, y PascalCase para nombrar clases. 
